c13239ab7e72b952df8246fe8e65465ec241fc1a
Gra Dzik <wiktor.gradzik@teb.edu.pl>
2024-11-27 21:47:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Modal, ModalHeader, ModalContent, ModalFooter } from './Modal';
 
-function App() {
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={toggleModal}>Kliknij</button>
+      <Modal isOpen={isOpen} toggle={toggleModal}>
+        <ModalHeader title="Tytuł Modala" />
+        <ModalContent>
+          <p>cos tam</p>
+        </ModalContent>
+        <ModalFooter callToActionLabel="Tak" />
+      </Modal>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Modal.js
@@ -0,0 +1,82 @@
+import React, { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import './Modal.css'; 
+
+const ModalContext = createContext();
+
+const Modal = ({ isOpen, toggle, children }) => {
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      toggle();
+    }
+  };
+
+  return (
+    <ModalContext.Provider value={{ toggle }}>
+      {isOpen && (
+        <div className="modal-overlay" onClick={handleOverlayClick}>
+          <div className="modal-content">
+            {children}
+          </div>
+        </div>
+      )}
+    </ModalContext.Provider>
+  );
+};
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  toggle: PropTypes.func.isRequired,
+  children: PropTypes.node,
+};
+
+
+const ModalHeader = ({ title }) => {
+  const { toggle } = React.useContext(ModalContext);
+
+  return (
+    <div className="modal-header">
+      <h3>{title}</h3>
+      <button onClick={toggle}>&times;</button>
+    </div>
+  );
+};
+
+//PropTypes w React służą do walidacji typów właściwości (props) przekazywanych do komponentów. 
+//Dzięki nim można upewnić się, że komponenty otrzymują prawidłowe dane, co pomaga w wykrywaniu błędów i ułatwia zrozumienie kodu. 
+//PropTypes pozwalają na określenie, jakie typy danych są oczekiwane, a także czy dana właściwość jest wymagana.
+
+
+ModalHeader.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+const ModalContent = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+ModalContent.propTypes = {
+  children: PropTypes.node,
+};
+
+const ModalFooter = ({ callToActionLabel }) => {
+  const { toggle } = React.useContext(ModalContext);
+
+  const handleCallToAction = () => {
+    alert('OK');
+    toggle();
+  };
+
+  return (
+    <div className="modal-footer">
+      <button onClick={toggle}>Anuluj</button>
+      <button onClick={handleCallToAction}>{callToActionLabel}</button>
+    </div>
+  );
+};
+
+ModalFooter.propTypes = {
+  callToActionLabel: PropTypes.string.isRequired,
+};
+
+export { Modal, ModalHeader, ModalContent, ModalFooter };
